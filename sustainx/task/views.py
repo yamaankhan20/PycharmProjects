26b6e300bf7993605a1c4c8ee96d5f767ff5f78e
yamaankhan20 <khanyamaan1@gmail.com>
2023-08-22 06:31:03 +0500

    done

--- sustainx/task/views.py
+++ sustainx/task/views.py
@@ -1,13 +1,31 @@
+from asyncio import tasks
+import asyncio
 from django.shortcuts import render
 from django.shortcuts import HttpResponse
+from django import forms
 # Create your views here.
 
-task=['foo','blaa','beat']
+all_task=['foo','blaa','beat']
+
+class NewTaskForms(forms.Form):
+    task = forms.CharField(label='new task')
+
 def index(request):
     return render(request, 'tasks-pages/index.html',{
-        'tasks': task
+        'tasks_list': all_task
     })
 
 
 def add_tasks(request):
-    return render(request, 'tasks-pages /add.html')
+    if request.method == 'POST':
+        form_detials = NewTaskForms(request.POST)
+        if form_detials.is_valid():
+            task_data = form_detials.cleaned_data['task']
+            all_task.append(task_data)
+        else:
+            return (request,'tasks-pages/add.html',{
+                'form': form_detials
+            })
+    return render(request, 'tasks-pages/add.html',{
+        'form': NewTaskForms()
+    })
