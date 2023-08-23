dbd2978f99eeb4f3b57636fba3120e88717360e0
yamaankhan20 <khanyamaan1@gmail.com>
2023-08-23 06:15:52 +0500

    done

--- sustainx/task/views.py
+++ sustainx/task/views.py
@@ -2,17 +2,20 @@ from asyncio import tasks
 import asyncio
 from django.shortcuts import render
 from django.shortcuts import HttpResponse
+from django.http import HttpResponseRedirect
 from django import forms
+from django.urls import reverse
 # Create your views here.
 
-all_task=['foo','blaa','beat']
 
 class NewTaskForms(forms.Form):
     task = forms.CharField(label='new task')
 
 def index(request):
+    if 'all_tasks' not in request.session:
+        request.session['all_tasks']=[]
     return render(request, 'tasks-pages/index.html',{
-        'tasks_list': all_task
+        'tasks_list': request.session['all_tasks']
     })
 
 
@@ -21,7 +24,8 @@ def add_tasks(request):
         form_detials = NewTaskForms(request.POST)
         if form_detials.is_valid():
             task_data = form_detials.cleaned_data['task']
-            all_task.append(task_data)
+            request.session['all_tasks'] += [task_data]
+            return HttpResponseRedirect(reverse('tasks:index'))
         else:
             return (request,'tasks-pages/add.html',{
                 'form': form_detials
